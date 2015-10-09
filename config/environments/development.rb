0b35f7a66b436106d104a8d77cf925437c3f5d7e
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-10-09 23:22:57 +0900

    Created an index html file in the public folder so that I could see an index page. I also adjusted settings to that I probably will not be able to send emails from this particular app although I will cross this bridge later.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,14 +32,14 @@ Rails.application.configure do
   domain: Rails.application.secrets.domain_name,
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: Rails,application.secrets.email_provider_username,
+  user_name: Rails.application.secrets.email_provider_username,
   password: Rails.application.secrets.email_provider_password
 }
   # ActionMailer config
   config.action_mailer.default_url_options = { :host => 'localhost: 3000'
   }
-  config.action_mailer.deliver_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.deliver_method = :smtp
+#   config.action_mailer.raise_delivery_errors = true
 
   # Send email in development mode?
   config.action_mailer.perform_deliveries = true
